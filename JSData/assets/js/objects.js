48ea641f084c22d6f6c8bcee4dfe233cba35fd4d
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-01 10:31:14 -0500

    Add Exercise 2 and JSON placeholder pages

--- JSData/assets/js/objects.js
+++ JSData/assets/js/objects.js
@@ -1,2 +1,33 @@
 console.log("Objects Script Loaded!");
 
+// Define an object using curly brackets
+let course = {
+  name: "JavaScript Applications",
+  awesome: true,
+  teachers: ["Assaf", "Shane"],
+  students: [
+    {
+      name: "Steve",
+      computer: {
+        OS: "Linux",
+        type: "laptop",
+      },
+    },
+  ],
+};
+
+// Access the object using dot notation
+console.log(course.name);
+
+// Access the object using bracket notation
+console.log(couse["awesome"]);
+
+// Nested brackets example
+console.log(course.students[0].computer.OS);
+
+// Updating: Properties of objects can be updated
+course.name = "super duper class";
+
+// Mutating: You can also assign entirely new keys, delete existing ones
+course.fun = true; //add a property
+delete course.name; //remove one
