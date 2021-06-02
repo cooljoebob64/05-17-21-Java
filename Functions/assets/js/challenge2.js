a8ed3c44271fd73b814c1df836e8e83b5a7a0d3d
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-02 09:37:52 -0500

    Add placeholder for challenge 2

---
+++ Functions/assets/js/challenge2.js
@@ -0,0 +1,10 @@
+// Challenge 2
+
+// Define a hands array with the values 'rock', 'paper', and 'scissors'
+// Define a function getComputerHand() that returns a hand from the array using parseInt(Math.random()*10) % 3)
+// Define a function called compare() that
+// Takes two choices as arguments
+// Determines the winner
+// Logs the hands played by each
+// Returns a string indicating the result
+// Play a game
