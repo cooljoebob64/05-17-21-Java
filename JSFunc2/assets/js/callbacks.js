5dad85e8646b0d1904a923af2691bb23eed8e92f
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-07 08:51:45 -0500

    Add code examples of JS Array functions

--- JSFunc2/assets/js/callbacks.js
+++ JSFunc2/assets/js/callbacks.js
@@ -2,9 +2,9 @@
 
 // Build our array of students
 let students = [
-  { firstName: "John Lennon", average: 90, assignmentsCompleted : 4 },
-  { firstName: "Ringo Starr", average: 58, assignmentsCompleted : 1 },
-  { firstName: "Paul McCartney", average: 82, assignmentsCompleted : 7 },
+  { firstName: "John Lennon", average: 90, assignmentsCompleted: 4 },
+  { firstName: "Ringo Starr", average: 58, assignmentsCompleted: 1 },
+  { firstName: "Paul McCartney", average: 82, assignmentsCompleted: 7 },
 ];
 console.log("The students:");
 console.log(students);
@@ -30,8 +30,33 @@ console.log("Full names: ");
 console.log(fullNames);
 
 // 4. Reduce - Calculate a single value based on results of the callback function for each item
+// Parameters for reduce are 1. The callback function, and 2. The initial value
 let totalAssignments = students.reduce(function (sum, current) {
-    return sum + current.assignmentsCompleted;
-})
-console.log("Total Assignments Completed:")
+  return sum + current.assignmentsCompleted;
+}, 0);
+console.log("Total Assignments Completed:");
 console.log(totalAssignments);
+
+let classAverage =
+  students.reduce(function (sum, current) {
+    return sum + current.average;
+  }, 0) / students.length;
+console.log("Class Average Grade:");
+console.log(classAverage);
+
+let statistics = students.reduce(
+  function (cumulativeStatistics, current) {
+    let newStatistics = {};
+    newStatistics.studentCount = cumulativeStatistics.studentCount + 1;
+    newStatistics.averageGrade =
+      (cumulativeStatistics.studentCount * cumulativeStatistics.averageGrade +
+        current.average) /
+      newStatistics.studentCount;
+    newStatistics.assignmentsCompleted =
+      cumulativeStatistics.assignmentsCompleted + current.assignmentsCompleted;
+    return newStatistics;
+  },
+  { studentCount: 0, averageGrade: 0, assignmentsCompleted: 0 }
+);
+console.log("Our statistics object:");
+console.log(statistics);
