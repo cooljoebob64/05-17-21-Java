c4d55d9a260d70ec988f933165694f88f8575ee1
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-07 08:52:02 -0500

    Add files for Challenge 1

---
+++ JSFunc2/assets/js/challenge1.js
@@ -0,0 +1,12 @@
+// Create an Array named superHeroes shown above
+// Create a secretIdentity variable
+// Assign superHeroes.map() to the secretIdentity variable
+// Assign an anonymous function to superheroes.map() as an argument
+// Your anonymous function should have one parameter named revealArray
+// Inside the block of your anonymous function:
+// You'll be working with revealArray as an argument
+// Using revealArray, return a string that will join the two array items.
+// join them together with the string "is" (ie: "Batman is Bruce Wayne")
+// Log the results to the screen and join them with a line break.
+
+
