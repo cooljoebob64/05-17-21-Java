ad8f0b3e3284f495fef92950386ca847ba80675c
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-07 11:08:40 -0500

    Add new functionality to slideshow in challenge 3

--- JSFunc2/assets/js/challenge3.js
+++ JSFunc2/assets/js/challenge3.js
@@ -10,45 +10,72 @@
 
 // Automatically pause the slideshow if it gets to the end of the photolist while playing.
 
-
 let slideshow = {
-    photoList: [],
-    addPhoto: function (newPhotoName) {
-      this.photoList.push(newPhotoName);
-      console.log(`Added photo: ${newPhotoName}`);
-    },
-    currentPhotoIndex: 0,
-    nextPhoto: function () {
+  photoList: [],
+  addPhoto: function (newPhotoName) {
+    this.photoList.push(newPhotoName);
+    console.log(`Added photo: ${newPhotoName}`);
+  },
+  currentPhotoIndex: 0,
+  nextPhoto: function () {
+    if (this.currentPhotoIndex > this.photoList.length - 2) {
+      console.log("End of slideshow.");
+      this.pause();
+    } else {
       this.currentPhotoIndex++;
-      if (this.currentPhotoIndex > this.photoList.length - 1) {
-        console.log("End of slideshow.");
-        this.currentPhotoIndex--;
-      } else {
-        this.getCurrentPhoto();
-      }
-    },
-    prevPhoto: function () {
-      this.currentPhotoIndex--;
-      if (this.currentPhotoIndex < 0) {
-        console.log("End of slideshow.");
-        this.currentPhotoIndex++;
-      } else {
-        this.getCurrentPhoto();
-      }
-    },
-    getCurrentPhoto: function () {
-      if (this.photoList.length == 0) {
-        console.log(`No photos in the list!`);
-      }
-      console.log(
-        `Photo #${this.currentPhotoIndex + 1}: ${
-          this.photoList[this.currentPhotoIndex]
-        }`
-      );
-    },
-    getList: function () {
-      console.log(this.photoList);
-    },
-  };
-  
-  
+      this.getCurrentPhoto();
+    }
+  },
+  prevPhoto: function () {
+    this.currentPhotoIndex--;
+    if (this.currentPhotoIndex < 0) {
+      console.log("End of slideshow.");
+      this.currentPhotoIndex++;
+    } else {
+      this.getCurrentPhoto();
+    }
+  },
+  getCurrentPhoto: function () {
+    if (this.photoList.length == 0) {
+      console.log(`No photos in the list!`);
+    }
+    console.log(
+      `Photo #${this.currentPhotoIndex + 1}: ${
+        this.photoList[this.currentPhotoIndex]
+      }`
+    );
+    currentPhotoName.innerHTML = this.currentPhoto();
+  },
+  currentPhoto: function () {
+    return this.photoList[this.currentPhotoIndex];
+  },
+  getList: function () {
+    console.log(this.photoList);
+  },
+  playInterval: null,
+  play: function () {
+    let self = this;
+    this.playInterval = setInterval(function () {
+      self.nextPhoto();
+    }, 2000);
+  },
+  pause: function () {
+    console.log("Pausing playback...");
+    clearInterval(this.playInterval);
+  },
+};
+
+slideshow.addPhoto("My Weekend");
+slideshow.addPhoto("A Boat");
+slideshow.addPhoto("That time it happened");
+
+let playButton = document.getElementById("playButton");
+let pauseButton = document.getElementById("pauseButton");
+let currentPhotoName = document.getElementById("currentPhotoName");
+let nextButton = document.getElementById("nextButton")
+playButton.addEventListener("click", slideshow.play.bind(slideshow));
+pauseButton.addEventListener("click", slideshow.pause.bind(slideshow));
+
+// slideshow.getCurrentPhoto();
+
+// slideshow.play();
