bc84931912ae452c4e081a12a19ae4ba68adf3b9
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-08 10:17:29 -0500

    Add initial functionality to particle demo

--- JSOOP/assets/js/particle.js
+++ JSOOP/assets/js/particle.js
@@ -1,4 +1,29 @@
 function Particle(startX, startY) {
   this.x = startX;
   this.y = startY;
+  this.velocity = { x: 0, y: 0 };
 }
+
+Particle.prototype = {
+  display: function () {
+    return `${this.x}, ${this.y}`;
+  },
+};
+
+let particles = [];
+
+let numParticle = 100;
+for (let i = 0; i < numParticle; i++) {
+  particles.push(new Particle(i, Math.ceil(Math.random() * 500)));
+  //   console.log(
+  //     `Added a new particle at x=${particles[i].x}, y=${particles[i].y}`
+  //   );
+  //   console.log(particles[i].display());
+}
+
+// console.log(particles);
+
+console.log("Particle has own x property: " + particles[0].hasOwnProperty("x"));
+console.log(
+  "Particle has own display property: " + particles[0].hasOwnProperty("display")
+);
